e22b727042be9f55fb9e933794da799177cfe890
Sam White <sam94white@gmail.com>
2021-12-06 16:33:06 +0000

    Better rotation of list

--- day_6/solver.py
+++ day_6/solver.py
@@ -7,11 +7,8 @@ def calc_growth(fish, days):
     for f in range(9):
         day_fish.append(fish.count(f))
     for i in range(days):
-        next_day_fish = [0]*9
-        for f in range(8): 
-            next_day_fish[f] = day_fish[f+1]
+        next_day_fish = day_fish[1:] + day_fish[:1]
         next_day_fish[6] += day_fish[0]
-        next_day_fish[8]  = day_fish[0]
         day_fish = next_day_fish
     return sum(day_fish)
 
