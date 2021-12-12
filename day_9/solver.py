d987ccedcf28b0cdeb14fed50808a8e79ba6ab4d
Sam White <sam94white@gmail.com>
2021-12-12 12:45:03 +0000

    Example data works - my input does not

--- day_9/solver.py
+++ day_9/solver.py
@@ -36,6 +36,16 @@ class grid:
             for point in row:
                 self.get_adjacent_points(point)
 
+def get_basin(point):
+    if point.val == 9:
+        return []
+    slope_points = [get_basin(pt) for pt in point.adjacent_points if pt.val == 1+point.val]
+    expanded_points = [p for l in slope_points for p in l] + [point]
+    if expanded_points != []:
+        return list(set(expanded_points))
+    else:
+        return [point]
+
 if __name__ == "__main__": 
     input_lines = open("input.txt").read().splitlines()
     input_ = [list(map(int, list(l))) for l in input_lines]
@@ -44,6 +54,16 @@ if __name__ == "__main__":
     grid_.get_all_adjacent_points()
 
     # part 1
-    print(sum([point.val+1 for row in grid_.contents for point in row if point.is_minimum()]))
+    minimum_points = [point for row in grid_.contents for point in row if point.is_minimum()]
+    print(sum([point.val+1 for point in minimum_points]))
 
     # part 2
+    basin_sizes = []
+    for point in minimum_points:
+        basin_sizes.append((point, len(get_basin(point))))
+
+    basin_sizes.sort(reverse=True, key = lambda x: x[1])
+    
+    #print(basin_sizes)
+    print([(basin[0].x, basin[0].y) for basin in basin_sizes[0:3]])
+    #print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
