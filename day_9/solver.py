8b090699a29820f4580d14439e9049d0d7f64f30
Sam White <sam94white@gmail.com>
2021-12-12 13:24:38 +0000

    Fixed. Code was almost there - was thrown by the example

--- day_9/solver.py
+++ day_9/solver.py
@@ -39,14 +39,15 @@ class grid:
 def get_basin(point):
     if point.val == 9:
         return []
-    slope_points = [get_basin(pt) for pt in point.adjacent_points if pt.val == 1+point.val]
+    slope_points = [get_basin(pt) for pt in point.adjacent_points if pt.val > point.val]
     expanded_points = [p for l in slope_points for p in l] + [point]
     if expanded_points != []:
         return list(set(expanded_points))
     else:
         return [point]
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+
     input_lines = open("input.txt").read().splitlines()
     input_ = [list(map(int, list(l))) for l in input_lines]
 
@@ -54,16 +55,15 @@ if __name__ == "__main__":
     grid_.get_all_adjacent_points()
 
     # part 1
-    minimum_points = [point for row in grid_.contents for point in row if point.is_minimum()]
+    minimum_points = [point for row in grid_.contents 
+            for point in row if point.is_minimum()]
     print(sum([point.val+1 for point in minimum_points]))
 
     # part 2
     basin_sizes = []
     for point in minimum_points:
-        basin_sizes.append((point, len(get_basin(point))))
+        basin_sizes.append(len(get_basin(point)))
 
-    basin_sizes.sort(reverse=True, key = lambda x: x[1])
-    
-    #print(basin_sizes)
-    print([(basin[0].x, basin[0].y) for basin in basin_sizes[0:3]])
-    #print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
+    basin_sizes.sort(reverse=True)
+
+    print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
